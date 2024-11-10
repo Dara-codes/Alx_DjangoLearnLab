74df32eb767c5120b56d935ed84286f5930c3db4
DaraJoe <deejayblinks@gmail.com>
2024-11-10 23:56:21 +0100

    task 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Book
+from django.views.generic import DetailView
+from .models import Library
 
-# Create your views here.
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Welcome to the Library Project</h1><p>Use the navigation to view books or library details.</p>")
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
