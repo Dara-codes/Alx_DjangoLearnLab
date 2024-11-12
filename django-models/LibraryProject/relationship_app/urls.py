6928e5814f4f202328463368c165eaba5876966b
DaraJoe <deejayblinks@gmail.com>
2024-11-12 22:01:01 +0100

    task4

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 from .views import admin_view, librarian_view, member_view
+from .views import list_books, add_book, edit_book, delete_book
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -13,6 +14,9 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', delete_book, name='delete_book'),
 ]
 
 
