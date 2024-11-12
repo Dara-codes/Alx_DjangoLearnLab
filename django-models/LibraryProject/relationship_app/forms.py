fa59dfa61fe51545d3cf902e344d5ae143004b4b
DaraJoe <deejayblinks@gmail.com>
2024-11-12 21:51:18 +0100

    task4

--- django-models/LibraryProject/relationship_app/forms.py
+++ django-models/LibraryProject/relationship_app/forms.py
@@ -1,10 +1,7 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from .models import Book
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-
+class BookForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        model = Book
+        fields = ['title', 'author', 'published_date']
