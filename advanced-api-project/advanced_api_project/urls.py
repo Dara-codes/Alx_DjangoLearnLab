c1ebac0544af456ea3862d685d3312037750a1e3
DaraJoe <deejayblinks@gmail.com>
2024-12-08 23:51:35 +0100

    correct task 1

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # include is imported here
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # Admin URL
+    path('api/', include('api.urls')),  # Include URLs from the 'api' app
 ]
