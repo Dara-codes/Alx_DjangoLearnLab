d9f66805df91951c8ce424d386448038357e675f
DaraJoe <deejayblinks@gmail.com>
2024-12-08 23:43:59 +0100

    correct task 1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+# List all books
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()  # Get all books
+    serializer_class = BookSerializer
+
+# Retrieve a single book by its ID
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()  # Get all books
+    serializer_class = BookSerializer
+
+# Create a new book
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()  # To create a book, start with all books
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can create books
+
+# Update an existing book
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()  # Retrieve the books
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can update books
+
+# Delete a book
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()  # Get all books
+    permission_classes = [IsAuthenticated]  # Only authenticated users can delete books
