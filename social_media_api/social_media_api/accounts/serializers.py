26524b38296c419c7d6f2279bf4e0f66b9bb8419
DaraJoe <deejayblinks@gmail.com>
2024-12-15 23:53:31 +0100

    task 0

--- social_media_api/social_media_api/accounts/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import CustomUser
-from django.contrib.auth import get_user_model
-
-class CustomUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
