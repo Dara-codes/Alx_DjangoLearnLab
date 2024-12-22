650a4323b1c335837482bd6dfa27d110afbec52f
DaraJoe <deejayblinks@gmail.com>
2024-12-22 20:37:16 +0100

    task0

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth import get_user_model
+# from rest_framework.authtoken.models import Token, serializers.CharField(), Token.objects.create, get_user_model().objects.create_user
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
-from rest_framework.authtoken.models import Token, serializers.CharField(), Token.objects.create, get_user_model().objects.create_user
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,12 +1,15 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
 from .serializers import CustomUserSerializer
+from rest_framework.decorators import api_view, permission_classes
+from .models import CustomUser
+
 
 class RegisterView(generics.CreateAPIView):
-    queryset = get_user_model().objects.all()
+    queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [permissions.AllowAny]
 
@@ -15,3 +18,27 @@ class CustomAuthToken(ObtainAuthToken):
         response = super().post(request, *args, **kwargs)
         token, created = Token.objects.get_or_create(user=response.data['user'])
         return Response({'token': token.key})
+
+
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def follow_user(request, user_id):
+    try:
+        user_to_follow = CustomUser.objects.get(id=user_id)
+    except CustomUser.DoesNotExist:
+        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    request.user.following.add(user_to_follow)
+    return Response({'status': 'followed'}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def unfollow_user(request, user_id):
+    try:
+        user_to_unfollow = CustomUser.objects.get(id=user_id)
+    except CustomUser.DoesNotExist:
+        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    request.user.following.remove(user_to_unfollow)
+    return Response({'status': 'unfollowed'}, status=status.HTTP_200_OK)
+
