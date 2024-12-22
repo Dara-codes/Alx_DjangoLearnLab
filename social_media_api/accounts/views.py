ed795562d904a03519805519f15a1be3cd5be0b5
DaraJoe <deejayblinks@gmail.com>
2024-12-22 20:39:09 +0100

    task2

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import api_view, permission_classes
 from .models import CustomUser
 
 
-class RegisterView(generics.CreateAPIView):
+class RegisterView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [permissions.AllowAny]
