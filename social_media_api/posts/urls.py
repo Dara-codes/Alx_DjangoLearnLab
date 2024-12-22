5263f149a1d62367c9295596fdde036317ca30e1
DaraJoe <deejayblinks@gmail.com>
2024-12-22 23:10:23 +0100

    task3

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -10,6 +10,6 @@ router.register(r'comments', CommentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', FeedView.as_view(), name='feed'),
-    path('<int:post_id>/like/', like_post, name='like_post'),
-    path('<int:post_id>/unlike/', unlike_post, name='unlike_post'),
+    path('<int:pk>/like/', like_post, name='like_post'),
+    path('<int:pk>/unlike/', unlike_post, name='unlike_post'),
 ]
